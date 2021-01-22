2de4a7593cb63556daf2c7630f4cab3b0ac5838d
Berk Sevgi <berksevgi95@hotmail.com>
2021-01-22 20:40:39 +0300

    Stars improved

--- src/components/Star/Star.js
+++ src/components/Star/Star.js
@@ -10,12 +10,18 @@ const Star = ({
     return (
         <div className={`star ${className || ''}`}>
             {rank && rank <= 5 && Array.apply(null, Array(rank)).map((star, index) => (
-                <div key={index} className="filled" />
+                <div key={index} className="filled">
+                    ✭
+                </div>
             ))}
             {rank && rank <= 5 ? Array.apply(null, Array(5 - rank)).map((star, index) => (
-                <div key={index} className="empty" />
+                <div key={index} className="empty">
+                    ✭
+                </div>
             )) : Array.apply(null, Array(5)).map((star, index) => (
-                <div key={index} className="empty" />
+                <div key={index} className="empty">
+                    ✭
+                </div>
             ))}
         </div>
     )
